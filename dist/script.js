604d24c35adfb32c72525421522bef6a8880b117
siwam <siwamsingh2003@gmail.com>
2023-11-17 20:03:26 +0530

    code: m2 created

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,26 @@
-
+// m2 content toggle 
+document.querySelector("#content").addEventListener("click", function (e) {
+    let clickedElement = e.target;
+    
+    if (
+      clickedElement.parentElement.firstElementChild === clickedElement && 
+      clickedElement.parentElement.tagName === 'LI' && clickedElement.parentElement.parentElement.id === 'content'
+    ) {
+      let ele = clickedElement.nextElementSibling;
+      let topicName = clickedElement;
+  
+      ele.classList.toggle("afterClick");
+  
+      if (ele.classList.contains("afterClick")) {
+        setTimeout(function () {
+          topicName.style.transition = "border-radius 0.1s";
+          topicName.style.borderRadius = "0.75rem 0.75rem 0 0";
+        }, 0);
+      } else {
+        setTimeout(function () {
+          topicName.style.transition = "border-radius 0.1s";
+          topicName.style.borderRadius = "0.75rem";
+        }, 600);
+      }
+    }
+  });
