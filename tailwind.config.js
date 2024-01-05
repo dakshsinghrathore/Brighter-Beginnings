7128efc701a42a2f5c94055b5ecb80c2b68745ad
Manish <manishkr061210@gmail.com>
2024-01-05 23:30:21 +0530

    About section is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
